016a13dc72282fff2eca8bdcf3afb885f3c259e7
Naohide Sano <namachapanda2005@gmail.com>
2024-04-05 14:13:48 +0900

    clean up

--- src/test/java/vavi/apps/epub/EPubModel.java
+++ src/test/java/vavi/apps/epub/EPubModel.java
@@ -12,7 +12,6 @@ import java.io.UncheckedIOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.stream.Stream;
